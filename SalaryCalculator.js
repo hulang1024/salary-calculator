95a7590c9fa0217fd9e05a6dab93d9d251629b20
hulang1024 <1013644379@qq.com>
2018-04-19 21:08:06 +0800

    update

--- SalaryCalculator.js
+++ SalaryCalculator.js
@@ -1,64 +1,122 @@
 ﻿
 function SalaryCalculator() {
 
-  //社保缴费基数，不同地区规定不同，要以工资总额为准。
+  //社保缴费基数,一般以工资总额为准
   var socialInsuranceBase = 0;
-
-  // 各类型社保的个人和单位各自承担比例
+  //各类型社保的个人和单位各自承担比例，不同地区规定不同
   var socialInsuranceOrgAndPersonalRatios = {
-    yanglao: {org: 0, personal: 0},   //养老
-    yiliao: {org: 0, personal: 0},    //医疗
-    shiye: {org: 0, personal: 0},     //失业
-    gongshang: {org: 0, personal: 0}, //工伤
-    shengyu: {org: 0, personal: 0}    //生育
+    yanglao: {org: 20 / 100, personal: 8 / 100},  //养老
+    yiliao: {org: 6 / 100, personal: 2 / 100},    //医疗
+    shiye: {org: 2 / 100, personal: 1 / 100},     //失业
+    gongshang: {org: 0.8 / 100, personal: 0},     //工伤
+    shengyu: {org: 1 / 100, personal: 0}          //生育
   };
+
+  //住房公积金缴费基数,一般以工资总额为准
+  var housingAccFundBase = 0;
+  //住房公积金个人和单位各自承担比例, 自定义
+  var housingAccFundOrgAndPersonalRatios = {
+    org: 5 / 100,
+    personal: 5 / 100
+  };
+
+  var personalHousingAccFundNotPay = false;
 
   this.setOptions = function(options) {
     socialInsuranceBase = options.socialInsuranceBase;
+    housingAccFundBase = options.housingAccFundBase;
+    personalHousingAccFundNotPay = options.personalHousingAccFundNotPay;
     $.extend(socialInsuranceOrgAndPersonalRatios, options.socialInsuranceOrgAndPersonalRatios);
+    $.extend(housingAccFundOrgAndPersonalRatios, options.housingAccFundOrgAndPersonalRatios);
   }
-  
-  this.calcSocialInsurancePays = function() {
-    var items = {};
-    
-    for (var type in socialInsuranceOrgAndPersonalRatios) {
-      items[type] = {
-        org: calcSIPay('org', type),
-        personal: calcSIPay('personal', type)
-      };
-    }
-    
+
+  this.getOptions = function() {
     return {
-      items: items,
-      sum: {
-        org: calcSIPay('org'),
-        personal: calcSIPay('personal')
-      }
+      socialInsuranceBase: socialInsuranceBase,
+      socialInsuranceOrgAndPersonalRatios: socialInsuranceOrgAndPersonalRatios,
+      housingAccFundBaseL: housingAccFundBase,
+      housingAccFundOrgAndPersonalRatios: housingAccFundOrgAndPersonalRatios,
+      personalHousingAccFundNotPay: personalHousingAccFundNotPay
     };
   }
-  
-  this.calcFinalSalary = function(salary) {
-    return salary - this.calcSocialInsurancePays().sum.personal;
+
+  this.calcPersonalFinalSalary = function(salary) {
+    var pays = this.calcPays();
+    var finalSalary = salary - pays.socialInsurance.sums.personal;
+    if (!personalHousingAccFundNotPay) {
+      finalSalary -= pays.housingAccFund.personal;
+    }
+
+    return finalSalary;
   }
-  
-  /*
-  应缴纳社保金额
-  @param personalOrOrg 个人还是单位
-  @param type 如为空,则表示总金额
-  */
-  function calcSIPay(personalOrOrg, type) {
-    var result = {};
-    var ratios = socialInsuranceOrgAndPersonalRatios;
-    if (type) {
-      return socialInsuranceBase * ratios[type][personalOrOrg];
-    } else {
+
+  /* 社保承担比例总计 */
+  this.calcSocialInsuranceRatioSums = function() {
+    function ratioSum(personalOrOrg) {
+      var ratios = socialInsuranceOrgAndPersonalRatios;
       var sum = 0;
-      // 遍历各类型社保
       for (var type in ratios) {
-        // 将个人比例结果累加
-        sum += socialInsuranceBase * ratios[type][personalOrOrg];
+        sum += ratios[type][personalOrOrg];
       }
       return sum;
     }
+
+    return {
+      org: ratioSum('org'),
+      personal: ratioSum('personal')
+    }
+  }
+
+  /* 计算社保和公积金的,个人和单位各自承担具体金额 */
+  this.calcPays = function() {
+
+    function calcSocialInsurancePays() {
+      function pay(type, personalOrOrg) {
+        return socialInsuranceBase * socialInsuranceOrgAndPersonalRatios[type][personalOrOrg];
+      }
+
+      function sumPay(personalOrOrg) {
+        var sum = 0;
+        // 遍历各类型社保, 将个人比例结果累加
+        for (var type in socialInsuranceOrgAndPersonalRatios) {
+          sum += pay(type, personalOrOrg);
+        }
+        return sum;
+      }
+
+      var items = {};
+
+      for (var type in socialInsuranceOrgAndPersonalRatios) {
+        items[type] = {
+          org: pay(type, 'org'),
+          personal: pay(type, 'personal')
+        };
+      }
+
+      return {
+        items: items,
+        sums: {
+          org: sumPay('org'),
+          personal: sumPay('personal')
+        }
+      };
+    }
+
+    function calcHousingAccFundPays() {
+      function pay(personalOrOrg) {
+        return housingAccFundBase * housingAccFundOrgAndPersonalRatios[personalOrOrg];
+      }
+
+      return {
+        org: pay('org'),
+        personal: personalHousingAccFundNotPay ? 0 : pay('personal')
+      };
+    }
+
+    return {
+      socialInsurance: calcSocialInsurancePays(),
+      housingAccFund: calcHousingAccFundPays()
+    };
+
   }
 }
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 ﻿var calculator = new SalaryCalculator();
 
-// 武汉的?
 var options = {
   socialInsuranceBase: 3093.30,
   socialInsuranceOrgAndPersonalRatios: {
@@ -9,15 +8,22 @@ var options = {
     shiye: {org: 0.64 / 100, personal: 0.2 / 100},
     gongshang: {org: 0.2 / 100, personal: 0},
     shengyu: {org: 0.85 / 100, personal: 0}
+  },
+  socialInsuranceBase: 1500,
+  housingAccFundOrgAndPersonalRatios: {
+    org: 5 / 100,
+    personal: 5 / 100
   }
 };
 
+calculator.setOptions(options);
+
 var typeTitles = {
-  yanglao: '养老保险金',
-  yiliao: '医疗保险金',
-  shiye: '失业保险金',
-  gongshang: '工伤保险金',
-  shengyu: '生育保险金'
+  yanglao: '养老保险',
+  yiliao: '医疗保险',
+  shiye: '失业保险',
+  gongshang: '工伤保险',
+  shengyu: '生育保险'
 };
 
 initSOAPRTable();
@@ -28,15 +34,30 @@ function initSOAPRTable() {
     + '<th>社保类型</th>'
     + '<th>单位承担比例</th>'
     + '<th>个人承担比例</th></tr>';
-    
+
   var ratios = options.socialInsuranceOrgAndPersonalRatios;
   for (var type in ratios) {
-    var trHtml = '<tr>'
-      + '<td>' + typeTitles[type] + '</td>' 
-      + '<td>' + (ratios[type].org * 100).toFixed(2) + '%' + '</td>' 
+    html += '<tr>'
+      + '<td>' + typeTitles[type] + '</td>'
+      + '<td>' + (ratios[type].org * 100).toFixed(2) + '%' + '</td>'
       + '<td>' + (ratios[type].personal * 100).toFixed(2) + '%' + '</td></tr>';
-    html += trHtml;
   }
+  html += '<tr>'
+    + '<td>' + '保险金总计' + '</td>'
+    + '<td>' + (calculator.calcSocialInsuranceRatioSums().org * 100).toFixed(2) + '%' + '</td>'
+    + '<td>' + (calculator.calcSocialInsuranceRatioSums().personal * 100).toFixed(2) + '%' + '</td></tr>';
+
+  html += '<tr>'
+    + '<th>公积金</th>'
+    + '<th>单位承担比例</th>'
+    + '<th>个人承担比例</th></tr>';
+
+  var ratios = options.housingAccFundOrgAndPersonalRatios;
+  html += '<tr>'
+    + '<td>公积金</td>'
+    + '<td>' + (ratios.org * 100).toFixed(2) + '%' + '</td>'
+    + '<td>' + (ratios.personal * 100).toFixed(2) + '%' + '</td></tr>';
+
   table.append(html);
 }
 
@@ -46,27 +67,43 @@ function drawPayTable(payInfo) {
     + '<th>社保类型</th>'
     + '<th>单位应缴</th>'
     + '<th>个人应缴</th></tr>';
-  
-  for (var type in payInfo.items) {
-    var trHtml = '<tr>'
-      + '<td>' + typeTitles[type] + '</td>' 
-      + '<td>' + (payInfo.items[type].org).toFixed(2) + '</td>' 
-      + '<td>' + (payInfo.items[type].personal).toFixed(2) + '</td></tr>';
-    html += trHtml;
+
+  for (var type in payInfo.socialInsurance.items) {
+    html += '<tr>'
+      + '<td>' + typeTitles[type] + '</td>'
+      + '<td>' + payInfo.socialInsurance.items[type].org.toFixed(2) + '</td>'
+      + '<td>' + payInfo.socialInsurance.items[type].personal.toFixed(2) + '</td></tr>';
   }
+
+  html += '<tr>'
+    + '<td>保险金总计</td>'
+    + '<td>' + payInfo.socialInsurance.sums.org.toFixed(2) + '</td>'
+    + '<td>' + payInfo.socialInsurance.sums.personal.toFixed(2) + '</td></tr>';
+
+  html += '<tr>'
+    + '<th>公积金</th>'
+    + '<th>单位应缴</th>'
+    + '<th>个人应缴</th></tr>';
+
+  html += '<tr>'
+    + '<td>公积金</td>'
+    + '<td>' + payInfo.housingAccFund.org.toFixed(2) + '</td>'
+    + '<td>' + payInfo.housingAccFund.personal.toFixed(2) + '</td></tr>';
   table.html(html);
 }
 
 $('#btnCalc').click(function() {
-  options.socialInsuranceBase = +$('#socialInsuranceBase').val();
+  options.socialInsuranceBase = +$('#socialInsuranceBase').val() || 0;
+  $('#socialInsuranceBase').val(options.socialInsuranceBase);
+  options.housingAccFundBase = +$('#housingAccFundBase').val() || 0;
+  $('#housingAccFundBase').val(options.housingAccFundBase);
+  options.personalHousingAccFundNotPay = $('#personalHousingAccFundNotPay').prop('checked');
+
   calculator.setOptions(options);
-  var result = calculator.calcSocialInsurancePays();
+  var result = calculator.calcPays();
   drawPayTable(result);
-  
+
   var originSalary = + $('#originSalary').val();
-  var finalSalary = calculator.calcFinalSalary(originSalary);
-  $('#finalSalary').val( finalSalary.toFixed(2) );
+  var finalSalary = calculator.calcPersonalFinalSalary(originSalary);
+  $('#finalSalary').text( finalSalary.toFixed(2) );
 });
-
-
-
