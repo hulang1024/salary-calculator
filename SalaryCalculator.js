e7286b42186b7cb81df3590a74814d478756581c
hlpp <1013644379@qq.com>
2018-04-19 18:21:56 +0800

    init

---
+++ SalaryCalculator.js
@@ -0,0 +1,64 @@
+﻿
+function SalaryCalculator() {
+
+  //社保缴费基数，不同地区规定不同，要以工资总额为准。
+  var socialInsuranceBase = 0;
+
+  // 各类型社保的个人和单位各自承担比例
+  var socialInsuranceOrgAndPersonalRatios = {
+    yanglao: {org: 0, personal: 0},   //养老
+    yiliao: {org: 0, personal: 0},    //医疗
+    shiye: {org: 0, personal: 0},     //失业
+    gongshang: {org: 0, personal: 0}, //工伤
+    shengyu: {org: 0, personal: 0}    //生育
+  };
+
+  this.setOptions = function(options) {
+    socialInsuranceBase = options.socialInsuranceBase;
+    $.extend(socialInsuranceOrgAndPersonalRatios, options.socialInsuranceOrgAndPersonalRatios);
+  }
+  
+  this.calcSocialInsurancePays = function() {
+    var items = {};
+    
+    for (var type in socialInsuranceOrgAndPersonalRatios) {
+      items[type] = {
+        org: calcSIPay('org', type),
+        personal: calcSIPay('personal', type)
+      };
+    }
+    
+    return {
+      items: items,
+      sum: {
+        org: calcSIPay('org'),
+        personal: calcSIPay('personal')
+      }
+    };
+  }
+  
+  this.calcFinalSalary = function(salary) {
+    return salary - this.calcSocialInsurancePays().sum.personal;
+  }
+  
+  /*
+  应缴纳社保金额
+  @param personalOrOrg 个人还是单位
+  @param type 如为空,则表示总金额
+  */
+  function calcSIPay(personalOrOrg, type) {
+    var result = {};
+    var ratios = socialInsuranceOrgAndPersonalRatios;
+    if (type) {
+      return socialInsuranceBase * ratios[type][personalOrOrg];
+    } else {
+      var sum = 0;
+      // 遍历各类型社保
+      for (var type in ratios) {
+        // 将个人比例结果累加
+        sum += socialInsuranceBase * ratios[type][personalOrOrg];
+      }
+      return sum;
+    }
+  }
+}
---
+++ main.js
@@ -0,0 +1,71 @@
+﻿var calculator = new SalaryCalculator();
+
+// 武汉的?
+var options = {
+  socialInsuranceBase: 3093.30,
+  socialInsuranceOrgAndPersonalRatios: {
+    yanglao: {org: 14 / 100, personal: 8 /100},
+    yiliao: {org: 7 / 100, personal: 2 / 100},
+    shiye: {org: 0.64 / 100, personal: 0.2 / 100},
+    gongshang: {org: 0.2 / 100, personal: 0},
+    shengyu: {org: 0.85 / 100, personal: 0}
+  }
+};
+
+var typeTitles = {
+  yanglao: '养老保险金',
+  yiliao: '医疗保险金',
+  shiye: '失业保险金',
+  gongshang: '工伤保险金',
+  shengyu: '生育保险金'
+};
+
+initSOAPRTable();
+
+function initSOAPRTable() {
+  var table = $('#table1');
+  var html = '<tr>'
+    + '<th>社保类型</th>'
+    + '<th>单位承担比例</th>'
+    + '<th>个人承担比例</th></tr>';
+    
+  var ratios = options.socialInsuranceOrgAndPersonalRatios;
+  for (var type in ratios) {
+    var trHtml = '<tr>'
+      + '<td>' + typeTitles[type] + '</td>' 
+      + '<td>' + (ratios[type].org * 100).toFixed(2) + '%' + '</td>' 
+      + '<td>' + (ratios[type].personal * 100).toFixed(2) + '%' + '</td></tr>';
+    html += trHtml;
+  }
+  table.append(html);
+}
+
+function drawPayTable(payInfo) {
+  var table = $('#table2');
+  var html = '<tr>'
+    + '<th>社保类型</th>'
+    + '<th>单位应缴</th>'
+    + '<th>个人应缴</th></tr>';
+  
+  for (var type in payInfo.items) {
+    var trHtml = '<tr>'
+      + '<td>' + typeTitles[type] + '</td>' 
+      + '<td>' + (payInfo.items[type].org).toFixed(2) + '</td>' 
+      + '<td>' + (payInfo.items[type].personal).toFixed(2) + '</td></tr>';
+    html += trHtml;
+  }
+  table.html(html);
+}
+
+$('#btnCalc').click(function() {
+  calculator.setOptions(options);
+  var result = calculator.calcSocialInsurancePays();
+  drawPayTable(result);
+  
+  var originSalary = + $('#originSalary').val();
+  var finalSalary = calculator.calcFinalSalary(originSalary);
+  $('#finalSalary').val( finalSalary.toFixed(2) );
+});
+
+
+
